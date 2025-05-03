d2bda84028372db45d13297237828178f65f8403
ViNN280801 <vladislav_semykin01@mail.ru>
2025-05-03 10:34:15 +0300

    tests(ini_good): implemented 18 different tests for the ini_good() function

---
+++ tests/helper.h
@@ -0,0 +1,154 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#define COLOR_RESET ""
+#define COLOR_INFO ""
+#define COLOR_SUCCESS ""
+#define COLOR_ERROR ""
+#define COLOR_WARNING ""
+#else
+#define COLOR_RESET "\033[0m"
+#define COLOR_INFO "\033[34m"
+#define COLOR_SUCCESS "\033[32m"
+#define COLOR_ERROR "\033[31m"
+#define COLOR_WARNING "\033[33m"
+#endif
+
+static void print_info(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+#ifdef _WIN32
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+    vfprintf_s(stdout, format, args);
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+#else
+    fprintf(stdout, "%s[INFO]%s ", COLOR_INFO, COLOR_RESET);
+    vfprintf(stdout, format, args);
+#endif
+    va_end(args);
+}
+
+static void print_success(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+#ifdef _WIN32
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+    vfprintf_s(stdout, format, args);
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+#else
+    fprintf(stdout, "%s[SUCCESS]%s ", COLOR_SUCCESS, COLOR_RESET);
+    vfprintf(stdout, format, args);
+#endif
+    va_end(args);
+}
+
+static void print_error(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+#ifdef _WIN32
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
+    vfprintf_s(stderr, format, args);
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+#else
+    fprintf(stderr, "%s[ERROR]%s ", COLOR_ERROR, COLOR_RESET);
+    vfprintf(stderr, format, args);
+#endif
+    va_end(args);
+}
+
+static void print_warning(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+#ifdef _WIN32
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+    vfprintf_s(stderr, format, args);
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+#else
+    fprintf(stderr, "%s[WARNING]%s ", COLOR_WARNING, COLOR_RESET);
+    vfprintf(stderr, format, args);
+#endif
+    va_end(args);
+}
+
+static void create_test_file(const char *filename, const char *content)
+{
+    FILE *file = fopen(filename, "w");
+    if (!file)
+    {
+        perror("Failed to create test file, do it manually after the test!");
+        exit(EXIT_FAILURE);
+    }
+    if (fprintf(file, "%s", content) < 0)
+    {
+        perror("Failed to write to test file");
+        exit(EXIT_FAILURE);
+    }
+    if (fclose(file) != 0)
+    {
+        perror("Failed to close test file, do it manually after the test!");
+        exit(EXIT_FAILURE);
+    }
+}
+
+static void remove_test_file(const char *filename)
+{
+#ifdef _WIN32
+    if (remove(filename) != 0)
+    {
+        perror("Failed to remove test file, do it manually after the test!");
+    }
+#else
+    if (unlink(filename) != 0)
+    {
+        perror("Failed to remove test file, do it manually after the test!");
+    }
+#endif
+}
+
+static void create_test_dir(const char *dirname)
+{
+#ifdef _WIN32
+    if (system("mkdir test_dir 2> nul") != 0)
+    {
+        perror("Failed to create test directory");
+        exit(EXIT_FAILURE);
+    }
+#else
+    if (system("mkdir -p test_dir") != 0)
+    {
+        perror("Failed to create test directory");
+        exit(EXIT_FAILURE);
+    }
+#endif
+}
+
+static void remove_test_dir(const char *dirname)
+{
+#ifdef _WIN32
+    if (system("rmdir /Q /S test_dir 2> nul") != 0)
+    {
+        perror("Failed to remove test directory");
+    }
+#else
+    if (system("rm -rf test_dir") != 0)
+    {
+        perror("Failed to remove test directory");
+    }
+#endif
+}
+
+#endif // HELPER_H
---
+++ tests/ini_good_tests.c
@@ -0,0 +1,317 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if INI_OS_UNIX
+#include <sys/stat.h>
+#endif
+
+#if INI_OS_WINDOWS
+#include <windows.h>
+#elif INI_OS_UNIX || INI_OS_MACOS
+#include <unistd.h>
+#endif
+
+#include "helper.h"
+#include "iniparser.h"
+
+void test_null_filepath()
+{
+    ini_error_details_t err = ini_good(NULL);
+    if (err.error != INI_INVALID_ARGUMENT)
+    {
+        print_error("test_null_filepath failed: expected INI_INVALID_ARGUMENT, got %d\n", err.error);
+        return;
+    }
+    print_success("test_null_filepath passed\n");
+}
+
+void test_nonexistent_file()
+{
+    ini_error_details_t err = ini_good("nonexistent.ini");
+    if (err.error != INI_FILE_NOT_FOUND)
+    {
+        print_error("test_nonexistent_file failed: expected INI_FILE_NOT_FOUND, got %d\n", err.error);
+        return;
+    }
+    print_success("test_nonexistent_file passed\n");
+}
+
+void test_directory()
+{
+    create_test_dir("test_dir");
+    ini_error_details_t err = ini_good("test_dir");
+    if (err.error != INI_FILE_IS_DIR)
+    {
+        print_error("test_directory failed: expected INI_FILE_IS_DIR, got %d\n", err.error);
+        remove_test_dir("test_dir");
+        return;
+    }
+    print_success("test_directory passed\n");
+    remove_test_dir("test_dir");
+}
+
+void test_empty_file()
+{
+    create_test_file("empty.ini", "");
+    ini_error_details_t err = ini_good("empty.ini");
+    if (err.error != INI_FILE_EMPTY)
+    {
+        print_error("test_empty_file failed: expected INI_FILE_EMPTY, got %d\n", err.error);
+        remove_test_file("empty.ini");
+        return;
+    }
+    print_success("test_empty_file passed\n");
+    remove_test_file("empty.ini");
+}
+
+void test_valid_file()
+{
+    create_test_file("valid.ini", "[section]\nkey=value\n");
+    ini_error_details_t err = ini_good("valid.ini");
+    if (err.error != INI_SUCCESS)
+    {
+        print_error("test_valid_file failed: expected INI_SUCCESS, got %d\n", err.error);
+        remove_test_file("valid.ini");
+        return;
+    }
+    print_success("test_valid_file passed\n");
+    remove_test_file("valid.ini");
+}
+
+void test_bad_format_missing_bracket()
+{
+    create_test_file("bad_missing_bracket.ini", "[section\nkey=value\n");
+    ini_error_details_t err = ini_good("bad_missing_bracket.ini");
+    if (err.error != INI_FILE_BAD_FORMAT)
+    {
+        print_error("test_bad_format_missing_bracket failed: expected INI_FILE_BAD_FORMAT, got %d\n", err.error);
+        remove_test_file("bad_missing_bracket.ini");
+        return;
+    }
+    print_success("test_bad_format_missing_bracket passed\n");
+    remove_test_file("bad_missing_bracket.ini");
+}
+
+void test_bad_format_empty_key()
+{
+    create_test_file("bad_empty_key.ini", "[section]\n=value\n");
+    ini_error_details_t err = ini_good("bad_empty_key.ini");
+    if (err.error != INI_FILE_BAD_FORMAT)
+    {
+        print_error("test_bad_format_empty_key failed: expected INI_FILE_BAD_FORMAT, got %d\n", err.error);
+        remove_test_file("bad_empty_key.ini");
+        return;
+    }
+    print_success("test_bad_format_empty_key passed\n");
+    remove_test_file("bad_empty_key.ini");
+}
+
+void test_bad_format_empty_value()
+{
+    create_test_file("bad_empty_value.ini", "[section]\nkey=\n");
+    ini_error_details_t err = ini_good("bad_empty_value.ini");
+    if (err.error != INI_FILE_BAD_FORMAT)
+    {
+        print_error("test_bad_format_empty_value failed: expected INI_FILE_BAD_FORMAT, got %d\n", err.error);
+        remove_test_file("bad_empty_value.ini");
+        return;
+    }
+    print_success("test_bad_format_empty_value passed\n");
+    remove_test_file("bad_empty_value.ini");
+}
+
+void test_bad_format_unbalanced_quotes()
+{
+    create_test_file("bad_unbalanced_quotes.ini", "[section]\nkey=\"value\n");
+    ini_error_details_t err = ini_good("bad_unbalanced_quotes.ini");
+    if (err.error != INI_FILE_BAD_FORMAT)
+    {
+        print_error("test_bad_format_unbalanced_quotes failed: expected INI_FILE_BAD_FORMAT, got %d\n", err.error);
+        remove_test_file("bad_unbalanced_quotes.ini");
+        return;
+    }
+    print_success("test_bad_format_unbalanced_quotes passed\n");
+    remove_test_file("bad_unbalanced_quotes.ini");
+}
+
+void test_bad_format_arrays_not_supported()
+{
+    create_test_file("bad_arrays.ini", "[section]\nkey=1,2,3\n");
+    ini_error_details_t err = ini_good("bad_arrays.ini");
+    if (err.error != INI_FILE_BAD_FORMAT)
+    {
+        print_error("test_bad_format_arrays_not_supported failed: expected INI_FILE_BAD_FORMAT, got %d\n", err.error);
+        remove_test_file("bad_arrays.ini");
+        return;
+    }
+    print_success("test_bad_format_arrays_not_supported passed\n");
+    remove_test_file("bad_arrays.ini");
+}
+
+void test_utf8_chars()
+{
+    create_test_file("utf8.ini", "[секция]\nключ=значение\n[节]\n键=值\n");
+    ini_error_details_t err = ini_good("utf8.ini");
+    if (err.error != INI_SUCCESS)
+    {
+        print_error("UTF-8 test failed");
+        remove_test_file("utf8.ini");
+        return;
+    }
+    print_success("test_utf8_chars passed\n");
+    remove_test_file("utf8.ini");
+}
+
+void test_windows_line_endings()
+{
+    create_test_file("windows.ini", "[section]\r\nkey=value\r\n");
+    ini_error_details_t err = ini_good("windows.ini");
+    if (err.error != INI_SUCCESS)
+    {
+        print_error("Windows line endings test failed: expected INI_SUCCESS, got %d\n", err.error);
+        remove_test_file("windows.ini");
+        return;
+    }
+    print_success("test_windows_line_endings passed\n");
+    remove_test_file("windows.ini");
+}
+
+void test_line_too_long()
+{
+    char long_line[INI_LINE_MAX + 2];
+    memset(long_line, 'a', INI_LINE_MAX + 1);
+
+    create_test_file("long_line.ini", long_line);
+    ini_error_details_t err = ini_good("long_line.ini");
+    if (err.error != INI_FILE_BAD_FORMAT)
+    {
+        print_error("test_line_too_long failed: expected INI_FILE_BAD_FORMAT, got %d\n", err.error);
+        remove_test_file("long_line.ini");
+        return;
+    }
+    print_success("test_line_too_long passed\n");
+    remove_test_file("long_line.ini");
+}
+
+void test_file_deleted_during_check()
+{
+    char const *filepath = "deleted_during_check.ini";
+    create_test_file(filepath, "[section]\nkey=value\n");
+    remove_test_file(filepath);
+    ini_error_details_t err = ini_good(filepath);
+    if (err.error != INI_FILE_NOT_FOUND)
+    {
+        print_error("test_file_deleted_during_check failed: expected INI_FILE_NOT_FOUND, got %d\n", err.error);
+        return;
+    }
+    print_success("test_file_deleted_during_check passed\n");
+}
+
+void test_binary_data()
+{
+    char const *filepath = "binary.ini";
+    FILE *fp = fopen(filepath, "wb");
+    if (!fp)
+    {
+        print_error("Failed to create binary file");
+        return;
+    }
+    unsigned char binary_data[] = {0x01, 0x02, 0x03, 0x00, 0xFF, 0xFE, 0xFD};
+    if (fwrite(binary_data, sizeof(binary_data), 1, fp) != 1)
+    {
+        print_error("Failed to write binary data to file");
+        fclose(fp);
+        remove_test_file(filepath);
+        return;
+    }
+    if (fclose(fp) != 0)
+    {
+        print_error("Failed to close binary file");
+        remove_test_file(filepath);
+        return;
+    }
+
+    ini_error_details_t err = ini_good(filepath);
+    if (err.error != INI_FILE_BAD_FORMAT)
+    {
+        print_error("test_binary_data failed: expected INI_FILE_BAD_FORMAT, got %d\n", err.error);
+        remove_test_file(filepath);
+        return;
+    }
+    print_success("test_binary_data passed\n");
+    remove_test_file(filepath);
+}
+
+#if INI_OS_LINUX
+void test_no_read_permission()
+{
+    create_test_file("no_read.ini", "[section]\nkey=value\n");
+    chmod("no_read.ini", 0333);
+    ini_error_details_t err = ini_good("no_read.ini");
+    if (err.error != INI_FILE_OPEN_FAILED)
+    {
+        print_error("Expected INI_FILE_OPEN_FAILED for no-read-permission file");
+        remove_test_file("no_read.ini");
+        return;
+    }
+    remove_test_file("no_read.ini");
+}
+
+void test_symlink()
+{
+    create_test_file("target.ini", "[section]\nkey=value\n");
+    symlink("target.ini", "symlink.ini");
+    ini_error_details_t err = ini_good("symlink.ini");
+    if (err.error != INI_SUCCESS)
+    {
+        print_error("Symlink test failed");
+        remove_test_file("symlink.ini");
+        remove_test_file("target.ini");
+        return;
+    }
+    remove_test_file("symlink.ini");
+    remove_test_file("target.ini");
+}
+
+void test_special_chars()
+{
+    create_test_file("special.ini", "[sec#tion]\nke=y=val\\;ue\n");
+    ini_error_details_t err = ini_good("special.ini");
+    if (err.error != INI_SUCCESS)
+    {
+        print_error("Special chars test failed");
+        remove_test_file("special.ini");
+        return;
+    }
+    remove_test_file("special.ini");
+}
+#endif
+
+int main()
+{
+    test_null_filepath();
+    test_nonexistent_file();
+    test_directory();
+    test_empty_file();
+    test_valid_file();
+    test_bad_format_missing_bracket();
+    test_bad_format_empty_key();
+    test_bad_format_empty_value();
+    test_bad_format_unbalanced_quotes();
+    test_bad_format_arrays_not_supported();
+    test_utf8_chars();
+    test_windows_line_endings();
+    test_line_too_long();
+    test_file_deleted_during_check();
+    test_binary_data();
+
+#if INI_OS_LINUX
+    test_no_read_permission();
+    test_symlink();
+    test_special_chars();
+#endif
+
+    print_success("All tests passed!\n");
+    return EXIT_SUCCESS;
+}
